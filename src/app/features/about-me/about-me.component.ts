4a8ea7f2fa6d2700b9b99663948a46cb8936d9ff
KeatonSmith <keatonsmith510@gmail.com>
2020-12-22 16:02:18 -0500

    Pretty now with more info

--- src/app/features/about-me/about-me.component.ts
+++ src/app/features/about-me/about-me.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WorkExperience } from '@src/app/models/work-experience.model';
 import * as work_experience_data from 'src/assets/json/work-experience.json';
+import * as extra_experience_data from 'src/assets/json/extra-resume-details.json';
 
 @Component({
   selector: 'ng-ns-about-me',
@@ -10,10 +11,10 @@ import * as work_experience_data from 'src/assets/json/work-experience.json';
 export class AboutMeComponent implements OnInit {
 
   workExperience: WorkExperience[] =  JSON.parse(JSON.stringify((work_experience_data as WorkExperience[])["default"]));
+  extraExperience: any = JSON.parse(JSON.stringify((extra_experience_data as any)["default"]));
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.workExperience);
   }
 
 }
