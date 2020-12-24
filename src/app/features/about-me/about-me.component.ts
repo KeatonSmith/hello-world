40949842a87d077284c3525b6701bf93078437fd
KeatonSmith <keatonsmith510@gmail.com>
2020-12-24 16:21:44 -0500

    Added projects and skills are WIP

--- src/app/features/about-me/about-me.component.ts
+++ src/app/features/about-me/about-me.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { WorkExperience } from '@src/app/models/work-experience.model';
 import * as work_experience_data from 'src/assets/json/work-experience.json';
 import * as extra_experience_data from 'src/assets/json/extra-resume-details.json';
+import * as personal_projects_data from 'src/assets/json/personal-projects.json';
 
 @Component({
   selector: 'ng-ns-about-me',
@@ -12,6 +13,8 @@ export class AboutMeComponent implements OnInit {
 
   workExperience: WorkExperience[] =  JSON.parse(JSON.stringify((work_experience_data as WorkExperience[])["default"]));
   extraExperience: any = JSON.parse(JSON.stringify((extra_experience_data as any)["default"]));
+  personalProjects: any[] =  JSON.parse(JSON.stringify((personal_projects_data as any[])["default"]));
+
   constructor() { }
 
   ngOnInit(): void {
